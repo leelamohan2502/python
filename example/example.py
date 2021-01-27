39370a753bfabbb1140da739bf6738d70c589e90
leelamohan2502 <78030236+leelamohan2502@users.noreply.github.com>
2021-01-27 22:18:04 +0530

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,1 +1,23 @@
-print('helloworld')
+# comments are indicate with hash
+#numbers are whole numbers it contains 0,1,2,3....
+n=12
+n=12.5
+print('numbers are {} \n'.format(n))
+#here we can understand python is dynamic data type
+#data operators
+# + indicates addition
+# - indicates subtraction
+# * mulitiplication
+# / division
+# % mod operator
+# ** power
+
+#*****************strings***********************
+
+#strings are immutable
+#and separed by silcing
+name="hello world"
+#letters can be access by indexing
+print("the first letter of string is {}\n".format(name[0]))
+
+
